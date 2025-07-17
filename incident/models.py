a71b0995ef07903aae2dd1e4d3a1bcf8db450317
@erick <erickescribaa@gmail.com>
2025-07-17 00:14:19 -0500

    feat: update incident class

--- incident/models.py
+++ incident/models.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-@dataclass(frozen=True)
+@dataclass(eq=True, frozen=True)
 class Incident:
     id: int
     type: str  # e.g., "infrastructure", "security", "application"
